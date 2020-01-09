7ac2601ce658d3beb51f5b44d81b04fce07b73d4
imxyb <xyb4638@gmail.com>
2020-01-09 23:05:24 +0800

    add uint32 slice sort
    Signed-off-by: imxyb <xyb4638@gmail.com>

--- sort/sort.go
+++ sort/sort.go
@@ -29,6 +29,10 @@ func Int32(slice []int32) {
 	sort.Sort(Int32Slice(slice))
 }
 
+func Uint32(slice []uint32) {
+	sort.Sort(Uint32Slice(slice))
+}
+
 type Int64Slice []int64
 
 func (p Int64Slice) Len() int {
@@ -54,5 +58,19 @@ func (p Int32Slice) Less(i, j int) bool {
 }
 
 func (p Int32Slice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+type Uint32Slice []uint32
+
+func (p Uint32Slice) Len() int {
+	return len(p)
+}
+
+func (p Uint32Slice) Less(i, j int) bool {
+	return p[i] < p[j]
+}
+
+func (p Uint32Slice) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
