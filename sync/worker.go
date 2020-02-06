fb01e04e0f2e9c85824467b8c0700ce251e6e252
huiren <zhrlnt@gmail.com>
2020-02-06 22:20:44 +0800

    comments
    Signed-off-by: huiren <zhrlnt@gmail.com>

--- sync/worker.go
+++ sync/worker.go
@@ -2,33 +2,38 @@ package gxsync
 
 import "context"
 
-type Task interface{}
-type FuncTask func()
-type CtxFuncTask func(context.Context)
-type StructTask struct{}
+type (
+	Task        interface{}           // Task is a job for worker handle
+	FuncTask    func()                // FuncTask functional task
+	CtxFuncTask func(context.Context) // CtxFuncTask is function task with context
+)
 
+// TaskQueue accept new job
 type TaskQueue interface {
 	Push(Task)
 	//Pop() Task
 }
 
+// Runner todo
 type Runner interface {
 	Start()
 	Wait()
 	Stop()
 }
 
+// Worker can consume jobs one by one
 type Worker interface {
 	TaskQueue
 	Runner
 }
 
+// WorkerManager accept jobs and dispatch to workers
 type WorkerManager interface {
 	TaskQueue
 	Runner
 }
 
-// PoolManager
+// PoolManager todo
 type PoolManager struct{}
 
 // PoolPreferManager is PoolManager with do task in new goroutine when pool is full.
