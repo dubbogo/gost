89232d381551d577d916ccf2f99e48fa6156fc74
wangwx <wangwx@tuya.com>
2020-11-13 15:32:03 +0800

    add comments for NewPoolDequeue

--- container/queue/poolqueue.go
+++ container/queue/poolqueue.go
@@ -198,6 +198,7 @@ func (d *poolDequeue) PopTail() (interface{}, bool) {
 	return val, true
 }
 
+// NewPoolDequeue new a poolDequeue instance.
 func NewPoolDequeue(n int) (*poolDequeue, error) {
 	if n&(n-1) != 0 {
 		return nil, errors.New("the size of pool must be a power of 2")
