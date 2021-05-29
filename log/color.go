2ca69c04df54868ab1a99b91634ffa0d9200e9b0
Gnps,Gwps <tangweihua163@gmail.com>
2021-05-29 10:44:09 +0800

    use Fprintf, not Fprint

--- log/color.go
+++ log/color.go
@@ -78,7 +78,7 @@ func CPrintf(color []byte, format string, args ...interface{}) {
 func CPrintfln(color []byte, format string, args ...interface{}) {
 	logStr := fmt.Sprintf(format, args...)
 	if isatty.IsTerminal(os.Stdout.Fd()) {
-		fmt.Fprint(os.Stdout, string(color)+funcFileLine()+"%s"+string(reset)+"\n", logStr)
+		fmt.Fprintf(os.Stdout, string(color)+funcFileLine()+"%s"+string(reset)+"\n", logStr)
 	} else {
 		fmt.Fprintf(os.Stdout, "%s\n", logStr)
 	}
