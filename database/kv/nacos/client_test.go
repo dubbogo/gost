47b4ea9932f9a4ae931a24825eb8d94ef21fdc0a
文徐 <wangwenxue.wwx@alibaba-inc.com>
2021-05-31 20:51:56 +0800

    fix nacos ut failed

--- database/kv/nacos/client_test.go
+++ database/kv/nacos/client_test.go
@@ -39,10 +39,12 @@ func TestNewNacosClient(t *testing.T) {
 
 	t.Run("naming_client", func(t *testing.T) {
 		client1, err := NewNacosNamingClient("nacos", true, scs, cc)
+		assert.Nil(t, err)
 		client2, err := NewNacosNamingClient("nacos", true, scs, cc)
+		assert.Nil(t, err)
 		client3, err := NewNacosNamingClient("nacos", false, scs, cc)
+		assert.Nil(t, err)
 		client4, err := NewNacosNamingClient("test", true, scs, cc)
-
 		assert.Nil(t, err)
 		assert.Equal(t, client1, client2)
 		assert.NotEqual(t, client1, client3)
