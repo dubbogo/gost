0d56b71e1e2d797a2b1a6c11555d1ac30ea355ed
zhaoyunxing <2385585770@qq.com>
2021-06-01 17:36:02 +0800

    up:config client and naming client
    add:nacos client
    
    add:nacos client
    
    add:ci
    
    add: assert err
    
    up:nacos address

--- database/kv/nacos/naming_client_test.go
+++ database/kv/nacos/naming_client_test.go
@@ -37,15 +37,17 @@ func TestNewNacosClient(t *testing.T) {
 		NotLoadCacheAtStart: true,
 	}
 
-	t.Run("naming_client", func(t *testing.T) {
-		client1, err := NewNacosNamingClient("nacos", true, scs, cc)
-		client2, err := NewNacosNamingClient("nacos", true, scs, cc)
-		client3, err := NewNacosNamingClient("nacos", false, scs, cc)
-		client4, err := NewNacosNamingClient("test", true, scs, cc)
+	client1, err := NewNacosNamingClient("nacos", true, scs, cc)
+	assert.Nil(t, err)
+	client2, err := NewNacosNamingClient("nacos", true, scs, cc)
+	assert.Nil(t, err)
+	client3, err := NewNacosNamingClient("nacos", false, scs, cc)
+	assert.Nil(t, err)
+	client4, err := NewNacosNamingClient("test", true, scs, cc)
+	assert.Nil(t, err)
 
-		assert.Nil(t, err)
-		assert.Equal(t, client1, client2)
-		assert.NotEqual(t, client1, client3)
-		assert.NotEqual(t, client1, client4)
-	})
+	assert.Equal(t, client1, client2)
+	assert.Equal(t, client1.activeCount, uint32(2))
+	assert.NotEqual(t, client1, client3)
+	assert.NotEqual(t, client1, client4)
 }
