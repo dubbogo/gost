fd9b88f342a2fe712a4daca02a1db1afba6e3c2a
赵云兴 <2385585770@qq.com>
2021-06-03 10:47:37 +0800

    Naming Client and Config Client (#59)
    * add:config client
    
    * up:config client and naming client
    
    add:nacos client
    
    add:nacos client
    
    add:ci
    
    add: assert err
    
    up:nacos address
    
    * up:lock name
    
    * add:note

--- database/kv/nacos/naming_client_test.go
+++ database/kv/nacos/naming_client_test.go
@@ -37,32 +37,17 @@ func TestNewNacosClient(t *testing.T) {
 		NotLoadCacheAtStart: true,
 	}
 
-	t.Run("naming_client", func(t *testing.T) {
-		client1, err := NewNacosNamingClient("nacos", true, scs, cc)
-		assert.Nil(t, err)
-		client2, err := NewNacosNamingClient("nacos", true, scs, cc)
-		assert.Nil(t, err)
-		client3, err := NewNacosNamingClient("nacos", false, scs, cc)
-		assert.Nil(t, err)
-		client4, err := NewNacosNamingClient("test", true, scs, cc)
-		assert.Nil(t, err)
-		assert.Equal(t, client1, client2)
-		assert.NotEqual(t, client1, client3)
-		assert.NotEqual(t, client1, client4)
-	})
+	client1, err := NewNacosNamingClient("nacos", true, scs, cc)
+	assert.Nil(t, err)
+	client2, err := NewNacosNamingClient("nacos", true, scs, cc)
+	assert.Nil(t, err)
+	client3, err := NewNacosNamingClient("nacos", false, scs, cc)
+	assert.Nil(t, err)
+	client4, err := NewNacosNamingClient("test", true, scs, cc)
+	assert.Nil(t, err)
 
-	t.Run("config_client", func(t *testing.T) {
-		client1, err := NewNacosConfigClient("nacos", true, scs, cc)
-		assert.Nil(t, err)
-		client2, err := NewNacosConfigClient("nacos", true, scs, cc)
-		assert.Nil(t, err)
-		client3, err := NewNacosConfigClient("nacos", false, scs, cc)
-		assert.Nil(t, err)
-		client4, err := NewNacosConfigClient("test", true, scs, cc)
-
-		assert.Nil(t, err)
-		assert.Equal(t, client1, client2)
-		assert.Equal(t, client1, client3)
-		assert.Equal(t, client1, client4)
-	})
+	assert.Equal(t, client1, client2)
+	assert.Equal(t, client1.activeCount, uint32(2))
+	assert.NotEqual(t, client1, client3)
+	assert.NotEqual(t, client1, client4)
 }
