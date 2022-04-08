c51589829b733ae19ae829825725623f6afb2b31
Zonglei Dong <dongzonglei@apache.org>
2022-04-08 09:55:17 +0800

    Fix checkstyle problem.

--- net/conncheck.go
+++ net/conncheck.go
@@ -26,6 +26,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
 package gxnet
 
 import (
@@ -70,3 +71,4 @@ func ConnCheck(conn net.Conn) error {
 
 	return sysErr
 }
+
