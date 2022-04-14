f1874e57e4074c78ce338eee84c91dd9b9f2a6dd
Zonglei Dong <dongzonglei@apache.org>
2022-04-14 19:53:58 +0800

    Fix method scope, private -> public.

--- net/conncheck_dummy.go
+++ net/conncheck_dummy.go
@@ -26,13 +26,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
 package gxnet
 
 import (
 	"net"
 )
 
-func connCheck(conn net.Conn) error {
+func ConnCheck(conn net.Conn) error {
 	return nil
 }
