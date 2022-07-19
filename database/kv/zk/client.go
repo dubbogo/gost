01d5bc07b11175091e46a464438e835500fa7991
澄潭 <ztywto@qq.com>
2022-07-19 13:56:48 +0800

    fix deadloop bug (#102)
    * fix deadloop bug
    
    * revert import

--- database/kv/zk/client.go
+++ database/kv/zk/client.go
@@ -215,12 +215,17 @@ func NewMockZookeeperClient(name string, timeout time.Duration, opts ...Option) 
 // HandleZkEvent handles zookeeper events
 func (d *DefaultHandler) HandleZkEvent(z *ZookeeperClient) {
 	var (
+		ok    bool
 		state int
 		event zk.Event
 	)
 	for {
 		select {
-		case event = <-z.Session:
+		case event, ok = <-z.Session:
+			if !ok {
+				// channel already closed
+				return
+			}
 			switch event.State {
 			case zk.StateDisconnected:
 				atomic.StoreUint32(&z.valid, 0)
